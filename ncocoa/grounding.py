b90407997b732055eeba664a07e76db9ad29975a
LBonassi95 <l.bonassi005@unibs.it>
2024-05-09 15:39:57 +0200

    Remove Quantifiers

--- ncocoa/grounding.py
+++ ncocoa/grounding.py
@@ -1,16 +1,22 @@
 from unified_planning.io.pddl_reader import PDDLReader
 from unified_planning.io.pddl_writer import PDDLWriter
 from unified_planning.model import Problem
-from unified_planning.engines.compilers.grounder import Grounder
+from unified_planning.engines import CompilationKind
 from unified_planning.engines.results import CompilerResult
 from numeric_tcore.achievers_helper import *
 from pathlib import Path
 
-
 def ground(domain_path: Path, problem_path: Path) -> Problem:
     reader = PDDLReader()
     problem = reader.parse_problem(domain_path, problem_path)
-    grounding_result: CompilerResult = Grounder().compile(
-            problem, CompilationKind.GROUNDING
-        )
-    return grounding_result.problem
+
+    grounder = Compiler(compilation_kind = CompilationKind.GROUNDING)
+    quantifier_remover = Compiler(compilation_kind = CompilationKind.QUANTIFIERS_REMOVING)
+
+    # Ground the problem
+    ground_problem = grounder.compile(problem).problem
+
+    # Remove Forall and Exists quantifiers
+    ground_problem = quantifier_remover.compile(ground_problem).problem
+
+    return ground_problem
