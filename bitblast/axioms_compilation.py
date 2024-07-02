b7e7f2daf2843fed63d411eadc598b461ea6593a
LBonassi95 <l.bonassi005@unibs.it>
2024-07-02 16:00:26 +0200

    Done full_adder_with_variables

--- bitblast/axioms_compilation.py
+++ bitblast/axioms_compilation.py
@@ -1,26 +1,6 @@
 from bitblast.helpers import *
 
 
-def carry_dp(i: int, label: str = None) -> Fluent:
-    return Fluent(f'c_{i}', BoolType()) if label is None else Fluent(f'c_{label}_{i}', BoolType())
-
-def sum_dp(i: int, label: str = None) -> Fluent:
-    return Fluent(f'z_{i}', BoolType()) if label is None else Fluent(f'z_{label}_{i}', BoolType())
-
-def compact_full_adder_circuit(x_bits: List[FNode], q_bits: List[FNode], label: str = None):
-    assert len(x_bits) == len(q_bits), "The number of bits in x and q must be the same"
-    circuit = {'c': {carry_dp(-1, label): FALSE()}, 'z': {}}
-
-    for i in range(len(x_bits)):
-        curr_carry = carry_dp(i, label)
-        curr_sum = sum_dp(i, label)
-        prev_carry = carry_dp(i-1, label)
-        circuit['z'][curr_sum] = XOr(XOr(x_bits[i], q_bits[i]), prev_carry()).simplify()
-        circuit['c'][curr_carry] = Or(And(x_bits[i], q_bits[i]), And(prev_carry(), XOr(x_bits[i], q_bits[i]))).simplify()
-
-    return circuit
-
-
 class Axiom:
     
     def __init__(self, head: Fluent, body: FNode):
@@ -73,9 +53,9 @@ class AxiomsCompiler:
             carry_axioms = []
             sum_axioms = []
             for i in range(-1, nbits):
-                carry_axioms += [Axiom(carry_dp(i, eff_label), circuit["c"][carry_dp(i, eff_label)])]
+                carry_axioms += [Axiom(carry_fl(i, eff_label), circuit["c"][carry_fl(i, eff_label)])]
                 if i >= 0:
-                    sum_axioms += [Axiom(sum_dp(i, eff_label), circuit["z"][sum_dp(i, eff_label)])]
+                    sum_axioms += [Axiom(sum_fl(i, eff_label), circuit["z"][sum_fl(i, eff_label)])]
             effects_axioms_map[eff] = carry_axioms + sum_axioms
         print("Compiling axioms...")
 
--- bitblast/helpers/full_adder_helper.py
+++ bitblast/helpers/full_adder_helper.py
@@ -12,3 +12,22 @@ def full_adder_circuit(x_bits: List[FNode], q_bits: List[FNode]):
         circuit['c'][i] = Or(And(x_bits[i], q_bits[i]), And(circuit["c"][i-1], XOr(x_bits[i], q_bits[i]))).simplify()
 
     return circuit
+
+def carry_fl(i: int, label: str = None) -> Fluent:
+    return Fluent(f'c_{i}', BoolType()) if label is None else Fluent(f'c_{label}_{i}', BoolType())
+
+def sum_fl(i: int, label: str = None) -> Fluent:
+    return Fluent(f'z_{i}', BoolType()) if label is None else Fluent(f'z_{label}_{i}', BoolType())
+
+def compact_full_adder_circuit(x_bits: List[FNode], q_bits: List[FNode], label: str = None):
+    assert len(x_bits) == len(q_bits), "The number of bits in x and q must be the same"
+    circuit = {'c': {carry_fl(-1, label): FALSE()}, 'z': {}}
+
+    for i in range(len(x_bits)):
+        curr_carry = carry_fl(i, label)
+        curr_sum = sum_fl(i, label)
+        prev_carry = carry_fl(i-1, label)
+        circuit['z'][curr_sum] = XOr(XOr(x_bits[i], q_bits[i]), prev_carry()).simplify()
+        circuit['c'][curr_carry] = Or(And(x_bits[i], q_bits[i]), And(prev_carry(), XOr(x_bits[i], q_bits[i]))).simplify()
+
+    return circuit
